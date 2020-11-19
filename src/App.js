46d3ef89c9e4d25f62ca868c652f59eaeddc1b2f
Guy Regev <guy.regev@luckyfishgames.com>
2020-11-19 22:36:20 +0200

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v66.g
+          Changed in the feature branch !
         </a>
       </header>
     </div>
